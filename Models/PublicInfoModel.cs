a801891ca0627e82a80ea204167b5573f86a43c8
BalazsNagyNET <chelseadrukker@gmail.com>
2016-12-16 22:39:58 +0100

    Fixed countdown problem with different language cultures.

--- Models/PublicInfoModel.cs
+++ Models/PublicInfoModel.cs
@@ -6,7 +6,7 @@ namespace Nop.Plugin.Misc.ComingSoonPage.Models
     public class PublicInfoModel : BaseNopModel
     {
         public string BackgroundUrl { get; set; }
-        public DateTime OpeningDate { get; set; }
+        public string OpeningDate { get; set; }
         public bool DisplayCountdown { get; set; }
         public bool DisplayNewsletterBox { get; set; }
         public bool DisplayLoginButton { get; set; }
