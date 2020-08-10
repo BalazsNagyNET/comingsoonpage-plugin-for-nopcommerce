1cf52baa8984c9823da699b1424ef72dfff0ee02
Balazs Nagy <balazsnagy@balazsnagy.net>
2020-08-10 20:33:38 +0200

    !!! ComingSoonPage PublicInfoModel extended with additional properties needed by login and subscription asp elements

--- Models/PublicInfoModel.cs
+++ Models/PublicInfoModel.cs
@@ -1,15 +1,45 @@
-﻿using Nop.Web.Framework.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Nop.Core.Domain.Customers;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Plugin.Misc.ComingSoonPage.Models
 {
     public class PublicInfoModel : BaseNopModel
     {
+        //ComingSoonPageProperties
         public string BackgroundUrl { get; set; }
         public string OpeningDate { get; set; }
         public bool DisplayCountdown { get; set; }
         public bool DisplayNewsletterBox { get; set; }
         public bool DisplayLoginButton { get; set; }
+
+        //LoginModel
+        [DataType(DataType.EmailAddress)]
+        [NopResourceDisplayName("Account.Login.Fields.Email")]
+        public string Email { get; set; }
+
         public bool UsernamesEnabled { get; set; }
+
+        public UserRegistrationType RegistrationType { get; set; }
+
+        [NopResourceDisplayName("Account.Login.Fields.Username")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [NoTrim]
+        [NopResourceDisplayName("Account.Login.Fields.Password")]
+        public string Password { get; set; }
+
+        [NopResourceDisplayName("Account.Login.Fields.RememberMe")]
+        public bool RememberMe { get; set; }
+
         public bool DisplayCaptcha { get; set; }
+
+        //NewsletterBoxModel
+        [DataType(DataType.EmailAddress)]
+        public string NewsletterEmail { get; set; }
+        public bool AllowToUnsubscribe { get; set; }
+
     }
 }
